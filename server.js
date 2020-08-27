89c114324e45538a88c339d0afd1fafa55c10753
alexlexpac <63712979+alexlexpac@users.noreply.github.com>
2020-08-27 16:34:29 +0530

    ini

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const dotenv = require('dotenv');
+// load env vars 
+dotenv.config({path:'./config/config.env'});
+const app = express();
+const PORT= process.env.PORT || 5000
+app.listen(PORT ,console.log(`server listenig in ${process.env.NODE_ENV} ON PORT NO ${PORT}`))
